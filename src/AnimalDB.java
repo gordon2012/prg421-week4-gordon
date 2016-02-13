9e87dabeb285780f746517cf369fdcd6a8ea9278
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-02-12 22:43:36 -0800

    implement rudimentary database functionality

--- src/AnimalDB.java
+++ src/AnimalDB.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.sql.*;
 public class AnimalDB
 {
@@ -20,12 +21,102 @@ public class AnimalDB
 		try
 		{
 			DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-
-			conn = DriverManager.getConnection("jdbc:derby:db/AnimalDB;create=true");
+			System.out.println("Driver registered");
 		}
 		catch(Exception e)
 		{
 			System.out.println("ERROR: " + e.getMessage());
 		}
+
+		try
+		{
+			conn = DriverManager.getConnection("jdbc:derby:db/AnimalDB;create=true");
+			System.out.println("Connection established");
+		}
+		catch(Exception e)
+		{
+			System.out.println("ERROR: " + e.getMessage());
+		}
+			// auto increment
+			// http://stackoverflow.com/a/4212603
+
+			// table exists?
+			// http://stackoverflow.com/a/5866339
+
+
+
+		Statement s;
+		try
+		{
+			s = conn.createStatement();
+			s.execute("CREATE TABLE Animals("
+//				+ "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
+				+ "id int, "
+				+ "name varchar(40), "
+				+ "color varchar(40), "
+				+ "legs int, "
+				+ "arms int, "
+				+ "tails int, "
+				+ "burrows boolean, "
+				+ "swims boolean, "
+				+ "flies boolean)");
+			System.out.println("Table created");
+		}
+		catch(SQLException e)
+		{
+			System.out.println("Table NOT created: " + e.getMessage());
+		}
+
+		String qry = "INSERT INTO Animals VALUES (1, 'Worm', 'Red', 0, 0, 1, true, true, false)";
+		Statement s2;
+		try
+		{
+			s2 = conn.createStatement();
+			s2.execute(qry);
+			System.out.println("Row created");
+		}
+		catch(Exception e)
+		{
+			System.out.println("ERROR: " + e.getMessage());
+		}
+			// PreparedStatement ps = conn.prepareStatement("INSERT INTO Animals VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			// ps.
+
+
+
 	}
+
+	List<String> listAnimals()
+	{
+		// Statement s;
+		List<String> result = new ArrayList<String>();
+		try
+		{
+			// s = conn.createStatement();
+			ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM Animals");
+			while(rs.next())
+			{
+				String res = "";
+				res+= rs.getInt("id") + "\t";
+				res+= rs.getString("name") + "\t";
+				res+= rs.getString("color") + "\t";
+				res+= rs.getInt("legs") + "\t";
+				res+= rs.getInt("arms") + "\t";
+				res+= rs.getInt("tails") + "\t";
+				res+= rs.getBoolean("burrows") + "\t";
+				res+= rs.getBoolean("swims") + "\t";
+				res+= rs.getBoolean("flies") + "\n";
+				result.add(res);
+			}
+
+		}
+		catch(Exception e)
+		{
+			System.out.println("ERROR: " + e.getMessage());
+		}
+		return result;
+	}
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Main
 {
 	public static void main(String[] args)
@@ -10,10 +11,18 @@ public class Main
 		int menu = 0;
 		do
 		{
-			ui.print("2/X\n");
+			ui.print("[1] List Animals\n[2] Add Animal\n[3] Remove Animal\n[X] Exit\n");
 
 			switch(ui.getInput("").substring(0).toUpperCase())
 			{
+				case "1":
+					List<String> animals = animalDB.listAnimals();
+					for(String a : animals)
+					{
+						ui.print(a);
+					}
+					break;
+
 				case "2":
 					Animal animal = new Animal(
 						ui.getInput("Name"),
